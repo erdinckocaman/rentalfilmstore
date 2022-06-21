9c99e37bb894d161b11ce34a4b31fe0cd8abcbce
Erdinc KOCAMAN <erdinc.kocaman@gmail.com>
2022-06-21 15:54:42 +0300

    arch rule added

---
+++ store-core/src/test/java/com/tamplan/sample/store/core/arch/RentalFilmStoreArchTest.java
@@ -0,0 +1,24 @@
+package com.tamplan.sample.store.core.arch;
+
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+
+@AnalyzeClasses(packages = "com.tamplan.sample.store", importOptions = {
+        ImportOption.DoNotIncludeTests.class,
+        ImportOption.DoNotIncludeJars.class
+})
+public class RentalFilmStoreArchTest {
+
+    @ArchTest
+    public static final ArchRule domainShouldNotHaveAnyDependenciesOtherThanJdkAndItself = ArchRuleDefinition
+            .classes().that().resideInAPackage("com.tamplan.sample.store.core.domain..")
+            .should().onlyDependOnClassesThat().resideInAnyPackage(
+                    "com.tamplan.sample.store.core.domain..",
+                    "com.tamplan.sample.store.core.commons..",
+                    "com.tamplan.sample.store.lib..",
+                    "java..");
+}
